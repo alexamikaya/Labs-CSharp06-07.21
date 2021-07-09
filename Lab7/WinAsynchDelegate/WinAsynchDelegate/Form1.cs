41c611270644554b5922c193411c6423c7654101
alexamikaya <alexamikaya@gmail.com>
2021-07-09 09:55:28 +0300

    1

---
+++ Lab7/WinAsynchDelegate/WinAsynchDelegate/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinAsynchDelegate
+{
+    public partial class Form1 : Form
+    {
+        bool Cancel;
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void TimeConsumingMethod(int seconds)
+        {
+            for (int j = 1; j <= seconds; j++)
+                SetProgress((int)(j * 100) / seconds);
+            if (Cancel)
+                break;
+            System.Threading.Thread.Sleep(1000);
+            
+            if (Cancel)
+            {
+                System.Windows.Forms.MessageBox.Show("Cancelled");
+                Cancel = false;
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("Complete");
+            }
+
+
+        }
+        private delegate void TimeConsumingMethodDelegate(int seconds);
+        public delegate void SetProgressDelegate(int val);
+        public void SetProgress(int val)
+        {
+            if (progressBar1.InvokeRequired)
+            {
+                SetProgressDelegate del = new SetProgressDelegate(SetProgress);
+                this.Invoke(del, new object[] { val });
+            }
+            else
+            {
+                progressBar1.Value = val;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Cancel)
+            {
+                System.Windows.Forms.MessageBox.Show("Cancelled");
+                Cancel = false;
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("Complete");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Cancel = true;
+        }
+    }
+}
---
+++ Lab7/WinAsynchMethod/WinAsynchMethod/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace WinAsynchMethod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.txbA = new System.Windows.Forms.TextBox();
+            this.txbB = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnWork = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(13, 13);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(65, 13);
+            this.lblA.TabIndex = 0;
+            this.lblA.Text = "Значение А";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(217, 13);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(65, 13);
+            this.lblB.TabIndex = 1;
+            this.lblB.Text = "Значение В";
+            // 
+            // txbA
+            // 
+            this.txbA.Location = new System.Drawing.Point(84, 12);
+            this.txbA.Name = "txbA";
+            this.txbA.Size = new System.Drawing.Size(100, 20);
+            this.txbA.TabIndex = 2;
+            // 
+            // txbB
+            // 
+            this.txbB.Location = new System.Drawing.Point(288, 13);
+            this.txbB.Name = "txbB";
+            this.txbB.Size = new System.Drawing.Size(100, 20);
+            this.txbB.TabIndex = 3;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(16, 65);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 4;
+            this.btnRun.Text = "Сумма";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnWork
+            // 
+            this.btnWork.Location = new System.Drawing.Point(119, 96);
+            this.btnWork.Name = "btnWork";
+            this.btnWork.Size = new System.Drawing.Size(75, 23);
+            this.btnWork.TabIndex = 5;
+            this.btnWork.Text = "Работа";
+            this.btnWork.UseVisualStyleBackColor = true;
+            this.btnWork.Click += new System.EventHandler(this.btnWork_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(220, 65);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(35, 13);
+            this.lblResult.TabIndex = 6;
+            this.lblResult.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 161);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnWork);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.txbB);
+            this.Controls.Add(this.txbA);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Name = "Form1";
+            this.Text = "Асинхронный запуск";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.TextBox txbA;
+        private System.Windows.Forms.TextBox txbB;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnWork;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
